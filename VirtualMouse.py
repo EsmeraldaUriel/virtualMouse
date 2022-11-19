08cdb064ef363e72980dba35ed8c359ac650e670
EsmeraldaUriel <116430431+EsmeraldaUriel@users.noreply.github.com>
2022-11-19 17:07:35 +0800

    "Hand Detector"

--- VirtualMouse.py
+++ VirtualMouse.py
@@ -1,7 +1,11 @@
 import cv2
+import mediapipe as mp
+
 cap = cv2.VideoCapture(0)
+hand_detector = mp.solutions.hands.Hands()
 
 while True:
     _, frame = cap.read()
+    rgb_frame = cv2.cvtColor(frame, cv2.BGR2RGB)
     cv2.imshow('Virtual Mouse', frame)
     cv2.waitKey(1)
