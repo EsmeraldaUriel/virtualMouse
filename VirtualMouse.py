76ad581ec160be0e2637a6e3a0de8e1089303f4f
EsmeraldaUriel <116430431+EsmeraldaUriel@users.noreply.github.com>
2022-11-19 17:47:51 +0800

    "Finalizing output by pyautogui"

--- VirtualMouse.py
+++ VirtualMouse.py
@@ -26,7 +26,6 @@ while True:
                     cv2.circle(img=frame, center=(x,y), radius=10, color=(0, 255, 255))
                     index_x = screen_width/frame_width*x
                     index_y = screen_height/frame_height*y
-                    pyautogui.moveTo(index_x, index_y)
                 if id == 4:
                     cv2.circle(img=frame, center=(x, y), radius=10, color=(0, 255, 255))
                     thumb_x = screen_width / frame_width * x
@@ -35,5 +34,7 @@ while True:
                     if abs(index_y - thumb_y) < 20:
                         pyautogui.click()
                         pyautogui.sleep(1)
+                    elif abs(index_y - thumb_y) < 100:
+                        pyautogui.moveTo(index_x, index_y)
     cv2.imshow('Virtual Mouse', frame)
     cv2.waitKey(1)
