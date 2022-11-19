7c3d05290150198a2226472967fcfdcf0b25616b
EsmeraldaUriel <116430431+EsmeraldaUriel@users.noreply.github.com>
2022-11-19 17:09:01 +0800

    "hand output"

--- VirtualMouse.py
+++ VirtualMouse.py
@@ -7,5 +7,8 @@ hand_detector = mp.solutions.hands.Hands()
 while True:
     _, frame = cap.read()
     rgb_frame = cv2.cvtColor(frame, cv2.BGR2RGB)
+    output = hand_detector.process(rgb_frame)
+    hands = output.multi_hand_landmarks
+    print(hands)
     cv2.imshow('Virtual Mouse', frame)
     cv2.waitKey(1)
